c70f9fad24878b7f81c1026061eb8d34653d08fa
Michael Senn <michael@morrolan.ch>
2016-08-20 16:10:24 +0200

    Adds timestamps to models, adds global Sequel plugins.
    - Adds created_at, updated_at and used_at timestamps to models
    - Adds plugin to automatically maintain created_at and updated_at
    - Adds plugin to provide question-mark style accessors for boolean
      columns
    - Enables raise_on_save_failure

--- config/database.rb
+++ config/database.rb
@@ -1,5 +1,9 @@
-Sequel::Model.plugin(:schema)
-Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
+Sequel::Model.plugin :boolean_readers
+Sequel::Model.plugin :schema
+Sequel::Model.plugin :timestamps
+
+Sequel::Model.raise_on_save_failure = true
+
 Sequel::Model.db = case Padrino.env
   when :development then Sequel.connect("sqlite://db/louis_xiv_development.db", :loggers => [logger])
   when :production  then Sequel.connect("sqlite://db/louis_xiv_production.db",  :loggers => [logger])
