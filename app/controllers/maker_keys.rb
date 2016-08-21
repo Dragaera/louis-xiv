97de56adfe08f4f280da8d0164a33cc443eec621
Michael Senn <michael@morrolan.ch>
2016-08-21 18:20:05 +0200

    Adds editing of maker keys.

--- app/controllers/maker_keys.rb
+++ app/controllers/maker_keys.rb
@@ -36,9 +36,28 @@ LouisXiv::App.controllers :maker_keys do
   end
 
   get :edit, map: '/maker_keys/:id/edit' do
+    @maker_key = session['maker_key'] || get_or_404(MakerKey, params.fetch('id').to_i)
+    session.delete 'maker_key'
+
+    render 'edit'
   end
 
   post :edit, map: '/maker_keys/:id/edit' do
+    maker_key = get_or_404(MakerKey, params.fetch('id').to_i)
+    maker_key_params = params.fetch('maker_key')
+
+    maker_key.name   = maker_key_params['name'] if maker_key_params.key? 'name'
+    maker_key.key    = maker_key_params['key']  if maker_key_params.key? 'key'
+    maker_key.active = to_bool(maker_key_params['active']) if maker_key_params.key? 'active'
+
+    if maker_key.valid?
+      maker_key.save
+      redirect(url(:maker_keys, :show, id: maker_key.id), success: "Modified #{ maker_key.name }")
+    else
+      # @Todo: Marshal: avoid full objects in session
+      session['maker_key'] = maker_key
+      redirect(url(:maker_keys, :edit, id: maker_key.id), form_error: pp_form_errors(maker_key.errors))
+    end
   end
 
   post :delete, map: '/maker_keys/:id/delete' do
