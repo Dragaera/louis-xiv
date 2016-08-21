6195a551b12f9c275f20de54bbfbfc99f2686912
Michael Senn <michael@morrolan.ch>
2016-08-21 19:32:37 +0200

    Adds maker event create action.

--- app/controllers/maker_events.rb
+++ app/controllers/maker_events.rb
@@ -6,9 +6,28 @@ LouisXiv::App.controllers :maker_events do
   end
 
   get :new do
+    @maker_event = session['maker_event'] || MakerEvent.new
+    session.delete 'maker_event'
+
+    render 'new'
   end
 
   post :new do
+    opts = params.fetch('maker_event')
+    maker_event = MakerEvent.new(
+      name:   opts.fetch('name'),
+      event:  opts.fetch('event'),
+      active: to_bool(opts.fetch('active'))
+    )
+
+    if maker_event.valid?
+      maker_event.save
+      redirect(url(:maker_events, :index), success: "Created Maker event #{ maker_event.name }")
+    else
+      # @Todo: Marshal
+      session['maker_event'] = maker_event
+      redirect(url(:maker_events, :new), form_error: pp_form_errors(maker_event.errors))
+    end
   end
 
   get :show, map: '/maker_events/:id' do
