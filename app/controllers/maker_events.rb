de7db48f8667b6d3eb4f708d29a143b27ac9cd05
Michael Senn <michael@morrolan.ch>
2016-08-21 19:42:21 +0200

    Adds maker event edit action.

--- app/controllers/maker_events.rb
+++ app/controllers/maker_events.rb
@@ -34,9 +34,28 @@ LouisXiv::App.controllers :maker_events do
   end
 
   get :edit, map: '/maker_events/:id/edit' do
+    @maker_event = session['maker_event'] || get_or_404(MakerEvent, params.fetch('id').to_i)
+    session.delete 'maker_event'
+
+    render 'edit'
   end
 
   post :edit, map: '/maker_events/:id/edit' do
+    maker_event = get_or_404(MakerEvent, params.fetch('id').to_i)
+    maker_event_params = params.fetch('maker_event')
+
+    maker_event.name   = maker_event_params['name'] if maker_event_params.key? 'name'
+    maker_event.event  = maker_event_params['event'] if maker_event_params.key? 'event'
+    maker_event.active = to_bool(maker_event_params['active']) if maker_event_params.key? 'active'
+
+    if maker_event.valid?
+      maker_event.save
+      redirect(url(:maker_events, :show, id: maker_event.id), success: "Modified #{ maker_event.name }")
+    else
+      # @Todo: Marshal
+      session['maker_event'] = maker_event
+      redirect(url(:maker_events, :edit, id: maker_event.id), form_error: pp_form_errors(maker_event.errors))
+    end
   end
 
   post :delete, map: '/maker_events/:id/delete' do
