657ea3ddc81c7e422e950490ccd694f32f6427e9
Michael Senn <michael@morrolan.ch>
2016-08-29 20:35:42 +0200

    Adds edit action to SolarLogStations controller.

--- app/controllers/solar_log_stations.rb
+++ app/controllers/solar_log_stations.rb
@@ -36,9 +36,30 @@ LouisXiv::App.controllers :solar_log_stations do
   end
 
   get :edit, map: '/solar_log_stations/:id/edit' do
+    @solar_log_station = session['solar_log_station'] || get_or_404(SolarLogStation, params.fetch('id').to_i)
+    session.delete 'solar_log_station'
+
+    render 'edit'
   end
 
   post :edit, map: '/solar_log_stations/:id/edit' do
+    solar_log_station = get_or_404(SolarLogStation, params.fetch('id').to_i)
+
+    obj_params = params.fetch('solar_log_station')
+    solar_log_station.set(
+      name: obj_params.fetch('name'),
+      http_url: obj_params.fetch('http_url'),
+      active: to_bool(obj_params.fetch('active', false))
+    )
+
+    if solar_log_station.valid?
+      solar_log_station.save
+      redirect(url(:solar_log_stations, :show, id: solar_log_station.id))
+    else
+      # @Todo: Marshal
+      session['solar_log_station'] = solar_log_station
+      redirect(url(:solar_log_stations, :edit, id: solar_log_station.id), form_error: pp_form_errors(solar_log_station.errors))
+    end
   end
 
   post :delete, map: '/solar_log_stations/:id/delete' do
