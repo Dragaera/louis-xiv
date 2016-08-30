5f58017e5172be4f1999a2bd582f0e6737887bc8
Michael Senn <michael@morrolan.ch>
2016-08-30 20:55:15 +0200

    Adds remaining fields of Sunscout::SolarLog::SolarLog to datapoint.
    Also renames some columns.

--- db/migrate/007_create_solar_log_data_points.rb
+++ db/migrate/007_create_solar_log_data_points.rb
@@ -4,15 +4,19 @@ Sequel.migration do
       primary_key :id
       foreign_key :solar_log_station_id, :solar_log_stations, null: false, on_update: :cascade, on_delete: :cascade
 
-      Int :power_ac
-      Int :power_dc
-
-      Int :power_total
+      Int   :power_ac
+      Int   :power_dc
+      Int   :power_max
+      Float :capacity
+      Float :efficiency
+      Int   :alternator_loss
 
       Int :voltage_ac
       Int :voltage_dc
 
-      Int :consumption_ac
+      Int   :consumption_ac
+      Float :usage
+      Int   :power_available
 
       Int :consumption_day
       Int :consumption_yesterday
@@ -20,13 +24,13 @@ Sequel.migration do
       Int :consumption_total
       Int :consumption_year
 
-      Int :yield_day
-      Int :yield_yesterday
-      Int :yield_month
-      Int :yield_total
-      Int :yield_year
+      Int :production_day
+      Int :production_yesterday
+      Int :production_month
+      Int :production_total
+      Int :production_year
 
-      DateTime :time
+      DateTime :timestamp
 
       DateTime :created_at
       DateTime :updated_at
--- models/solar_log_data_point.rb
+++ models/solar_log_data_point.rb
@@ -7,28 +7,33 @@ class SolarLogDataPoint < Sequel::Model
     s = Sunscout::SolarLog::SolarLog.new(solar_log_station.http_url)
 
     set(
-      power_ac: s.power_ac,
-      power_dc: s.power_dc,
-
-      power_total: s.power_total,
+      power_ac:        s.power_ac,
+      power_dc:        s.power_dc,
+      power_max:       s.power_total,
+      capacity:        s.capacity,
+      efficiency:      s.efficiency,
+      alternator_loss: s.alternator_loss,
 
       voltage_ac: s.voltage_ac,
       voltage_dc: s.voltage_dc,
 
-      consumption_ac: s.consumption_ac,
-      consumption_day: s.consumption_day,
+      consumption_ac:        s.consumption_ac,
+      usage:                 s.usage,
+      power_available:       s.power_available,
+
+      consumption_day:       s.consumption_day,
       consumption_yesterday: s.consumption_yesterday,
-      consumption_month: s.consumption_month,
-      consumption_year: s.consumption_year,
-      consumption_total: s.consumption_total,
+      consumption_month:     s.consumption_month,
+      consumption_year:      s.consumption_year,
+      consumption_total:     s.consumption_total,
 
-      yield_day: s.yield_day,
-      yield_yesterday: s.yield_yesterday,
-      yield_month: s.yield_month,
-      yield_year: s.yield_year,
-      yield_total: s.yield_total,
+      production_day:       s.yield_day,
+      production_yesterday: s.yield_yesterday,
+      production_month:     s.yield_month,
+      production_year:      s.yield_year,
+      production_total:     s.yield_total,
 
-      time: s.time
+      timestamp: s.time
     )
     save
   end
