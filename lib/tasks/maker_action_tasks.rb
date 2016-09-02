f41021eb42f6cbb3cafd6621f314942832c36c83
Michael Senn <michael@morrolan.ch>
2016-09-02 21:43:16 +0200

    Refactors MakerAction tasks.
    - Freeze string assigned to constant
    - Extract functionality into dedicated methods

--- lib/tasks/maker_action_tasks.rb
+++ lib/tasks/maker_action_tasks.rb
@@ -1,12 +1,11 @@
 module Tasks
   class CallMakerEvent
-    URI = 'https://maker.ifttt.com/trigger/%s/with/key/%s'
+    URI = 'https://maker.ifttt.com/trigger/%s/with/key/%s'.freeze
 
     @queue = :tasks
 
     def self.perform(event_id, key_id)
       event = MakerEvent[event_id]
-
       if event.nil?
         raise KeyError, "No MakerEvent with id '#{ event_id.inspect }'"
       end
@@ -15,8 +14,14 @@ module Tasks
       raise KeyError, "No MakerKey with id '#{ key_id.inspect }'" if key.nil?
 
       logger.info "Sending '#{ event.name }' (#{ event.event }) to key '#{ key.name }'"
+      post_to_maker(event, key)
 
-      uri = URI % [event.event, key.key]
+      touch_models(event, key)
+    end
+
+    private_class_method
+    def self.post_to_maker(event, key)
+      uri = format(URI, event.event, key.key)
       logger.debug "POSTing to #{ uri }"
 
       # @Todo: Error handling / logging
@@ -25,10 +30,12 @@ module Tasks
       logger.debug "Response body: #{ response.body }"
       logger.debug "Response message: #{ response.message }"
       logger.debug "Response headers: #{ response.headers.inspect }"
+    end
 
+    def self.touch_models(event, key)
       now = DateTime.now
-      key.update(used_at: now)
       event.update(used_at: now)
+      key.update(used_at: now)
     end
   end
 end
