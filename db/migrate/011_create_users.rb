6e0142aad2b245fd9545fa8dfad60d7985e218f4
Michael Senn <michael@morrolan.ch>
2016-09-13 17:18:42 +0200

    Adds User model and specs.
    Passwords hashed with BCrypt.

---
+++ db/migrate/011_create_users.rb
@@ -0,0 +1,18 @@
+Sequel.migration do
+  up do
+    create_table :users do
+      primary_key :id
+
+      String   :user,     null: false, unique: true
+      String   :password, null: false
+      Bool     :active,   null: false, default: true
+      DateTime :used_at
+      DateTime :created_at
+      DateTime :updated_at
+    end
+  end
+
+  down do
+    drop_table :users
+  end
+end
