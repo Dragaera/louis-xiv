42e1cf12f2026d993d5af39523273aeafc8aa1f3
Michael Senn <michael@morrolan.ch>
2016-09-15 20:15:06 +0200

    Forcibly sign user out if his session's user is inactive or nonexistant.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,6 +4,7 @@ module LouisXiv
   class App
     module UsersHelper
       def login
+        verify_user!
         if session.key? 'login_user'
           session['login_user']
         else
@@ -12,10 +13,22 @@ module LouisXiv
       end
 
       def logged_in?
+        verify_user!
         session['login_user']
       end
     end
 
     helpers UsersHelper
+
+    private
+    def verify_user!
+      if session.key? 'login_user'
+        session_user = session['login_user']
+        db_user      = User[session_user.id]
+        if db_user.nil? || !db_user.active?
+          session.delete 'login_user'
+        end
+      end
+    end
   end
 end
